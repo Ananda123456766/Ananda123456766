c92a3c46d1bd01a3bd12558f26b4a3cc49593237
Ananda123456766 <nandoffc01@gmail.com>
2024-07-20 23:16:24 +0700

    Create ip-nand.js

---
+++ ip-nand.js
@@ -0,0 +1,14 @@
+{
+  "allowedIps": ["114.79.17.40", "114.79.23.77"],
+  "users": [
+    {
+      "username": "Nand",
+      "password": "Nand34"
+    },
+    {
+      "username": "nand",
+      "password": "nand123"
+    }
+  ]
+}
+
